8df392dda33ffe2cb6e9b5cb75006fa62f669709
kriskok95 <kriskobota@abv.bg>
2020-03-03 22:03:35 +0200

    Updated the modal only when it is shown

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Aux from "../../../hoc/Auxiliary";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OrderSummary extends Component {
+  //This could be a functional component
+  render(){
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+  
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the followin ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Toal Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue with checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
     );
-  });
-
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the followin ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Toal Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue with checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
+  }
 };
 
-export default orderSummary;
+export default OrderSummary;
