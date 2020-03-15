fa9f8e8787de466365406bb36aae52d53fdfe650
kriskok95 <kriskobota@abv.bg>
2020-03-15 12:56:30 +0200

    Complete the checkout and save the order into DB

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 import Button from "../../../components/UI/Button/Button";
+import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.css";
+import axios from "../../../axios-orders";
 
 class ContactData extends Component {
   state = {
@@ -10,20 +12,59 @@ class ContactData extends Component {
     address: {
       street: "",
       postalCode: ""
-    }
+    },
+    loading: false,
   };
 
+
+  orderHandler = (event) =>  {
+      event.preventDefault();
+      this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Kristian Slavchev",
+        address: {
+          street: "testStreeet 1",
+          zipCode: "24324",
+          country: "Bulgaria"
+        },
+        email: "test@123.bg"
+      },
+      deliveryMethod: "fastest"
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then(response => {
+        this.setState({ loading: false});
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        this.setState({ loading: false});
+      });
+  }
+
   render() {
-    return (
-      <div className={classes.ContactData}>
-        <h4>Enter your contract Data</h4>
+    let form = (
         <form>
           <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
           <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
           <input className={classes.Input} type="text" name="street" placeholder="Street" />
           <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success">ORDER</Button>
+          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
+    );
+
+    if(this.state.loading){
+        form = <Spinner/>
+    }
+
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your contract Data</h4>
+        {form}
       </div>
     );
   }
