6047bf44b6549c26482d36607364ae59d40f073a
kriskok95 <kriskobota@abv.bg>
2020-03-15 17:27:16 +0200

    Made dynamically create input based on js config

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -4,62 +4,112 @@ import Button from "../../../components/UI/Button/Button";
 import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
-import Input from '../../../components/UI/Input/Input';
+import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends Component {
   state = {
-    name: "",
-    email: "",
-    address: {
-      street: "",
-      postalCode: ""
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name"
+        },
+        value: ""
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street"
+        },
+        value: ""
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "ZIP Code"
+        },
+        value: ""
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country"
+        },
+        value: ""
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your E-Mail"
+        },
+        value: ""
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" }
+          ]
+        },
+        value: ""
+      }
     },
-    loading: false,
+    loading: false
   };
 
-
-  orderHandler = (event) =>  {
-      event.preventDefault();
-      this.setState({ loading: true });
+  orderHandler = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price,
-      customer: {
-        name: "Kristian Slavchev",
-        address: {
-          street: "testStreeet 1",
-          zipCode: "24324",
-          country: "Bulgaria"
-        },
-        email: "test@123.bg"
-      },
-      deliveryMethod: "fastest"
+      price: this.props.price
     };
 
     axios
       .post("/orders.json", order)
       .then(response => {
-        this.setState({ loading: false});
-        this.props.history.push('/');
+        this.setState({ loading: false });
+        this.props.history.push("/");
       })
       .catch(error => {
-        this.setState({ loading: false});
+        this.setState({ loading: false });
       });
-  }
+  };
 
   render() {
+    const formElementsArray = [];
+
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
-        <form>
-          <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-          <Input inputtype="input" type="text" name="street" placeholder="Street" />
-          <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-          <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-        </form>
+      <form>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
     );
 
-    if(this.state.loading){
-        form = <Spinner/>
+    if (this.state.loading) {
+      form = <Spinner />;
     }
 
     return (
